44b886cf9671ff0fb02371e94e627fdce33044b5
lrzjason <lrzjason@users.noreply.github.com>
2024-07-15 12:02:40 +0800

    update

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-rem Ask if the user wants to setup venv
+echo 0. Setup venv
 set /p setup_venv=Do you want to setup venv? [Y/n]:
 if "!setup_venv!" == "y" (
    python -m venv venv
@@ -9,20 +9,22 @@ if "!setup_venv!" == "y" (
    call venv\Scripts\activate
 )
 
-rem Upgrade pip
+
+echo 1. Upgrade pip
 python -m pip install --upgrade pip
 
-rem Ask if the user wants to install torch
+
+echo 2. Install torch
 set /p install_torch=Do you want to install torch? [Y/n]:
 if "!install_torch!" == "y" (
    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 )
 
-rem Install other dependencies from requirements.txt
+echo 3. Install other dependencies from requirements.txt
 pip install -r requirements.txt
 
-rem Ask if the user wants to install the model file
-set /p install_model=Do you want to install the model file? (y/n):
+echo 4. Download the Model File
+set /p install_model=Do you want to download the model file? (y/n):
 if "!install_model!" == "y" (
     REM Try running huggingface-cli with --version
     huggingface-cli --version >nul 2>&1
