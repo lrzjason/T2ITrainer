7f25574e161a939ce315a1313812c9fbd2835203
lrzjason <lrzjason@users.noreply.github.com>
2024-10-25 00:28:09 +0800

    update

--- test/test_latents.py
+++ test/test_latents.py
@@ -11,17 +11,25 @@ import cv2
 import torchvision.transforms as T
 import torchvision
 
-vae_path = "F:/models/VAE/sdxl_vae_fp16fix.safetensors"
-vae = AutoencoderKL.from_single_file(
-    vae_path
+# vae_path = "F:/models/VAE/sdxl_vae_fp16fix.safetensors"
+vae_path = "F:/models/VAE/SD3_16c_vae.safetensors"
+# vae = AutoencoderKL.from_single_file(
+#     vae_path
+# )
+
+
+vae = AutoencoderKL.from_pretrained(
+    "F:/T2ITrainer/sd3.5L",
+    subfolder="vae",
 )
 
 
 vae.to("cuda").to(torch.float16)
 # npz_path = "F:/ImageSet/vit_train/anatomy/train/good_anatomy/4_prompt_res_1344x1344 - Copy (2).nplatent"
 # npz_path = "F:/ImageSet/kolors_cosplay/train/maileji/maileji_1.nplatent"
-npz_path = "F:/ImageSet/kolors_cosplay/train/Azami Onlyfans/Azami - 2B [20P]/01.nplatent"
+# npz_path = "F:/ImageSet/kolors_cosplay/train/Azami Onlyfans/Azami - 2B [20P]/01.nplatent"
 # image_path = "alan-w-ZpmFJoWRqUE-unsplash.webp"
+npz_path = "F:/ImageSet/sd35/diverse-photo-3740/0001.npsd35latent"
 
 latents = torch.load(npz_path)
 latent = latents['latent'].to("cuda").unsqueeze(0).to(torch.float16)
